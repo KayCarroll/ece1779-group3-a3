5f864611cb0b273fa6d11b1aa4aca060193bcd09
KayCarroll <kaycarrollmcneil@gmail.com>
2023-04-10 13:14:37 -0400

    updated time format

--- administration_app/adminapp/constants.py
+++ administration_app/adminapp/constants.py
@@ -1,4 +1,4 @@
 AWS_REGION_NAME = 'us-east-1'
 LOG_FORMAT = '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s'
-TIME_FORMAT = '%H:%M:%S'
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 VOTING_INFO_TABLE = 'voting_info'
